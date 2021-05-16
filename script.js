5bcd1d81c6dc063de2d4514b846af66dd38c3332
Rohan Batra <rohanbatra24@gmail.com>
2021-05-16 14:24:27 -0400

    attach event listeners to menu

---
+++ script.js
@@ -0,0 +1,22 @@
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const nav5 = document.getElementById("nav-5");
+
+// Toggle menu bar
+
+const toggleNav = (params) => {
+  menuBars.classList.toggle("change");
+};
+
+// Event listeners
+
+menuBars.addEventListener("click", toggleNav);
+nav1.addEventListener("click", toggleNav);
+nav2.addEventListener("click", toggleNav);
+nav3.addEventListener("click", toggleNav);
+nav4.addEventListener("click", toggleNav);
+nav5.addEventListener("click", toggleNav);
